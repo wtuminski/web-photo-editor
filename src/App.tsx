e2f15b7969f6d617bd738503f18e7b67c7adead6
Tumiński <wiktor.tuminski@macbook-wiktor.home>
2023-01-06 16:39:18 +0100

    Introduced simple app bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-import { Unstable_Grid2 as Grid } from '@mui/material';
+import { AppBar, Typography, Unstable_Grid2 as Grid } from '@mui/material';
 
 import { EditorView } from '~/Views/EditorView';
 
 export const App = () => (
-  <Grid container alignSelf="stretch" flexDirection="column" flex={1} p={4}>
-    <h1>Web Photo Editor</h1>
-    <EditorView />
+  <Grid container alignSelf="stretch" flexDirection="column" flex={1}>
+    <AppBar position="sticky">
+      <Typography color="inherit" component="p" variant="h5" px={4} py={1.5}>
+        Web Photo Editor
+      </Typography>
+    </AppBar>
+    <Grid display="flex" flexGrow={1} p={4}>
+      <EditorView />
+    </Grid>
   </Grid>
 );
