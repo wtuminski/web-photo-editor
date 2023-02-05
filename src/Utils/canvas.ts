cf98b12b1ba3ad9789c96fc32bf5d0936710c59b
Wiktor Tuminski <wiktor.tuminski@gmail.com>
2023-02-05 19:38:39 +0100

    Move local canvas drawing utils to Utils/canvas.ts and introduce a new function scheduleImageDrawingInCanvas

--- src/Utils/canvas.ts
+++ src/Utils/canvas.ts
@@ -11,3 +11,71 @@ export function get2dContext(
     : document.querySelector(selector);
   return (isHTMLCanvasElement(element) && element.getContext('2d')) || null;
 }
+
+const getImageURLAndCleanUp = (
+  imageFile: ImageFile,
+): [imageURL: string, cleanUp: CleanUpFunction] => {
+  const imageURL = URL.createObjectURL(imageFile);
+  return [imageURL, () => URL.revokeObjectURL(imageURL)];
+};
+
+const scheduleImageDrawing = (
+  imageURL: string,
+  drawImage: (image: HTMLImageElement) => void,
+): CleanUpFunction => {
+  const abortController = new AbortController();
+  const image = new Image();
+
+  image.addEventListener('load', () => drawImage(image), { signal: abortController.signal });
+  image.setAttribute('src', imageURL); // TODO - should it be moved somewhere else? (it's a hack
+
+  return () => abortController.abort();
+};
+
+const getCanvasMaxDimensions = (
+  context: CanvasRenderingContext2D,
+): { maxWidth: number; maxHeight: number } => {
+  const maxImageCoverage = 0.8;
+  const { height: parentHeight, width: parentWidth } =
+    context.canvas.parentElement!.getBoundingClientRect();
+
+  return { maxHeight: parentHeight * maxImageCoverage, maxWidth: parentWidth * maxImageCoverage };
+};
+
+const getNewCanvasDimensions = (
+  context: CanvasRenderingContext2D,
+  image: HTMLImageElement,
+): [width: number, height: number] => {
+  const { maxWidth, maxHeight } = getCanvasMaxDimensions(context);
+  const { naturalWidth: imageWidth, naturalHeight: imageHeight } = image;
+  const ratio = imageWidth / imageHeight;
+
+  // TODO - test this logic
+  if (imageHeight > maxHeight) return [maxHeight * ratio, maxHeight];
+  if (imageWidth > maxWidth) return [maxWidth, maxWidth / ratio];
+  return [imageWidth, imageHeight];
+};
+
+const drawImageInContext = (context: CanvasRenderingContext2D, image: HTMLImageElement) => {
+  const [newWidth, newHeight] = getNewCanvasDimensions(context, image);
+  context.canvas.setAttribute('width', newWidth.toString());
+  context.canvas.setAttribute('height', newHeight.toString());
+  context.drawImage(image, 0, 0, newWidth, newHeight);
+};
+
+export const scheduleImageDrawingInCanvas = (
+  canvasElement: HTMLCanvasElement,
+  imageFile: ImageFile,
+): CleanUpFunction | void => {
+  const context = get2dContext(canvasElement);
+
+  if (!context) return undefined;
+
+  const [imageURL, URLCleanUp] = getImageURLAndCleanUp(imageFile);
+  const drawCleanUp = scheduleImageDrawing(imageURL, drawImageInContext.bind(null, context));
+
+  return () => {
+    URLCleanUp();
+    drawCleanUp();
+  };
+};
--- src/Views/EditorView/components/ImageDisplaySpace/ImageDisplaySpace.tsx
+++ src/Views/EditorView/components/ImageDisplaySpace/ImageDisplaySpace.tsx
@@ -1,10 +1,7 @@
-import { flow } from '@mobily/ts-belt';
 import { Unstable_Grid2 as Grid } from '@mui/material';
 import { useEffect, useRef } from 'react';
 
-import { get2dContext } from '~/Utils/canvas';
-
-import { drawImageInContext, getImageURLAndCleanUp, scheduleImageDrawing } from './utils';
+import { scheduleImageDrawingInCanvas } from '~/Utils/canvas';
 
 interface Props {
   imageFile?: ImageFile;
@@ -12,18 +9,9 @@ interface Props {
 
 export const ImageDisplaySpace: React.FC<Props> = ({ imageFile }) => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
-
   useEffect(() => {
     if (!canvasRef.current || !imageFile) return undefined;
-
-    const context = get2dContext(canvasRef.current);
-
-    if (!context) return undefined;
-
-    const [imageURL, URLCleanUp] = getImageURLAndCleanUp(imageFile);
-    const drawCleanUp = scheduleImageDrawing(imageURL, drawImageInContext.bind(null, context));
-
-    return flow(URLCleanUp, drawCleanUp);
+    return scheduleImageDrawingInCanvas(canvasRef.current, imageFile);
   }, [imageFile, canvasRef]);
 
   return (
