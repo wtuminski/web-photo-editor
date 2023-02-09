0c0987fcd475c679575cb28fb392373cd3ef7b18
Wiktor Tuminski <wiktor.tuminski@gmail.com>
2023-02-09 20:36:38 +0100

    Update vite and setup coverage tests

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,19 @@
+/// <reference types="vitest" />
+
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       '~': path.resolve(__dirname, 'src'),
+    },
+  },
+  test: {
+    coverage: {
+      all: true,
     },
   },
 });
