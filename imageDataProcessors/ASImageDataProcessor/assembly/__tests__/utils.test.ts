b46588879ef7c4bce37d25ae1bd648fef4294493
Wiktor Tuminski <wiktor.tuminski@gmail.com>
2023-03-02 14:30:08 +0100

    Introduce new utils and tests for them

--- imageDataProcessors/ASImageDataProcessor/assembly/__tests__/utils.test.ts
+++ imageDataProcessors/ASImageDataProcessor/assembly/__tests__/utils.test.ts
@@ -1,4 +1,14 @@
-import { convertRgbaToRgbZeroOneRange, getMax, getMin, round } from '../utils';
+import {
+  adjustValueToRange,
+  average,
+  calculateFilterRate,
+  convertRgbaToRgbZeroOneRange,
+  getMax,
+  getMin,
+  mapPixels,
+  round,
+} from '../utils';
+import { mapPixelsInputArray, mapPixelsOutputArray } from './utils.mocks';
 
 describe('utils', () => {
   test('round', () => {
@@ -32,5 +42,66 @@ describe('utils', () => {
       ];
       expect(value).toBe(convertedValues[index]);
     });
+  });
+
+  test('adjustValueToRange', () => {
+    expect(adjustValueToRange({ maxValue: 360, minValue: 0, value: 200 })).toBe(200);
+    expect(adjustValueToRange({ maxValue: 360, minValue: 0, value: 500 })).toBe(360);
+    expect(adjustValueToRange({ maxValue: 360, minValue: 0, value: -10 })).toBe(0);
+    expect(adjustValueToRange({ maxValue: 25.5, minValue: -15.4, value: -10.1 })).toBe(-10.1);
+    expect(adjustValueToRange({ maxValue: 25.5, minValue: -15.4, value: 26.1 })).toBe(25.5);
+    expect(adjustValueToRange({ maxValue: 25.5, minValue: -15.4, value: -16.4 })).toBe(-15.4);
+  });
+
+  test('average', () => {
+    expect(average([1, 2, 5])).toBe(2.66666667);
+    expect(average([10, 22, 255])).toBe(95.66666667);
+    expect(average([0, 128, 255])).toBe(127.66666667);
+    expect(average([0, 0, 0])).toBe(0);
+    expect(average([255, 255, 255])).toBe(255);
+  });
+
+  test('calculateFilterRate', () => {
+    expect(calculateFilterRate(10)).toBe(1.1);
+    expect(calculateFilterRate(99)).toBe(1.99);
+    expect(calculateFilterRate(0)).toBe(1);
+    expect(calculateFilterRate(127)).toBe(2.27);
+    expect(calculateFilterRate(-128)).toBe(-0.27999997);
+    expect(calculateFilterRate(-11)).toBe(0.89);
+  });
+
+  test('mapPixels', () => {
+    // given
+    const input = new Uint8ClampedArray(mapPixelsInputArray.length);
+    input.set(mapPixelsInputArray);
+    const output = new Uint8ClampedArray(mapPixelsOutputArray.length);
+    output.set(mapPixelsOutputArray);
+
+    // when
+    const potentialOutput = mapPixels(input, 50, (rgba, filterValue) => {
+      const r = rgba[0];
+      const g = rgba[1];
+      const b = rgba[2];
+      const a = rgba[3];
+      const filterRate = calculateFilterRate(filterValue);
+      return [
+        round(
+          adjustValueToRange({
+            maxValue: 255,
+            minValue: 0,
+            value: r * filterRate,
+          }),
+        ) as u8,
+        round(adjustValueToRange({ maxValue: 255, minValue: 0, value: g * filterRate })) as u8,
+        round(adjustValueToRange({ maxValue: 255, minValue: 0, value: b * filterRate })) as u8,
+        a,
+      ];
+    });
+
+    // then
+    expect(Reflect.equals(potentialOutput, output)).toBe(
+      Reflect.SUCCESSFUL_MATCH,
+      `${potentialOutput.toString()} should be ${output.toString()}`,
+    );
   });
 });
--- imageDataProcessors/ASImageDataProcessor/assembly/utils.ts
+++ imageDataProcessors/ASImageDataProcessor/assembly/utils.ts
@@ -3,11 +3,36 @@ import { RGBAPixel, RGBPixel } from './types';
 //
 // constants
 //
-const MAX_RGB_VALUE: u8 = 255;
+const PIXEL_LENGTH = 4;
+export const MAX_RGB_VALUE: u8 = 255;
+export const MIN_RGB_VALUE: u8 = 0;
 
 //
-// helpers
+// private
 //
+const getPixel = (data: Uint8ClampedArray, startingIndex: u32): RGBAPixel => {
+  const r = data[startingIndex];
+  const g = data[startingIndex + 1];
+  const b = data[startingIndex + 2];
+  const a = data[startingIndex + 3];
+
+  return [r, g, b, a];
+};
+
+//
+// public
+//
+
+//
+// Math helpers
+class AdjustValueToRangeProps {
+  public value!: f32;
+  public maxValue!: f32;
+  public minValue!: f32;
+}
+export const adjustValueToRange = (a: AdjustValueToRangeProps): f32 =>
+  max(a.minValue, min(a.maxValue, a.value));
+
 export const round = (value: f32, decimals: u8 = 0): f32 =>
   Mathf.round(value * 10 ** decimals) / 10 ** decimals;
 
@@ -15,5 +40,36 @@ export const getMax = (values: StaticArray<f32>): f32 => values.sort().at(-1);
 
 export const getMin = (values: StaticArray<f32>): f32 => values.sort()[0];
 
+export const average = (values: u8[]): f32 =>
+  (values.reduce((sum, currentValue) => sum + currentValue, 0) as f32) / (values.length as f32);
+
+//
+// Transformation helpers
+
 export const convertRgbaToRgbZeroOneRange = (rgba: RGBAPixel): RGBPixel =>
   StaticArray.fromArray(rgba.slice(0, 3).map((color: u8): f32 => (color as f32) / MAX_RGB_VALUE));
+
+export const calculateFilterRate = (filterValue: i8): f32 => 1 + (filterValue as f32) / 100;
+
+// mapPixles isn't written in a functional way due to performance issues (e.g. lack of proper tail call optimization)
+export const mapPixels = (
+  data: Uint8ClampedArray,
+  filterValue: i8,
+  callbackfn: (RGBAPixel: RGBAPixel, filterValue: i8) => RGBAPixel,
+): Uint8ClampedArray => {
+  const newData = new Uint8ClampedArray(data.length);
+
+  for (let currentIndex = 0; currentIndex < data.length; currentIndex += PIXEL_LENGTH) {
+    const currentPixel = getPixel(data, currentIndex);
+    const rgba = callbackfn(currentPixel, filterValue);
+
+    /* eslint-disable immutable/no-mutation */
+    newData[currentIndex] = rgba[0];
+    newData[currentIndex + 1] = rgba[1];
+    newData[currentIndex + 2] = rgba[2];
+    newData[currentIndex + 3] = rgba[3];
+    /* eslint-enable immutable/no-mutation */
+  }
+
+  return newData;
+};
