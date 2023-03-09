316b6091d16f11365afab1a077a7a76e9182a2be
Wiktor Tuminski <wiktor.tuminski@gmail.com>
2023-03-09 13:58:42 +0100

    Move pixel processors to packages dir and remove utils package

--- packages/utils/src/constants.ts
+++
@@ -1,2 +0,0 @@
-export const MAX_RGB_VALUE = 255;
-export const MIN_RGB_VALUE = 0;
--- packages/utils/src/types.ts
+++
@@ -1,3 +0,0 @@
-export type RGBAPixel = [red: number, green: number, blue: number, alpha: number];
-
-export type HSLAPixel = [hue: number, saturation: number, luminosity: number, alpha: number];
